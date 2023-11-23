ff6586bf41eed9e44e81e46c0078651d7a9847a3
fed <fedor.fomichev@phystech.edu>
2023-11-22 16:29:05 -0800

    add solution

--- Top Interview 150/Matrix/Medium/54. Spiral Matrix/solution.py
+++ Top Interview 150/Matrix/Medium/54. Spiral Matrix/solution.py
@@ -5,57 +5,56 @@ def flatten_spirally(matrix: List[List[int]]) -> List[int]:
     """
     Iterate through all elements in matrix, and keep track on left border, right border, upper border, lower border,
     direction in which we're currently moving. If we reach any border, change direction and shrink certain border.
-    See image in Readme.md.
+    (See image in Readme.md)
 
     Time complexity: O(m*n)
     Space complexity: O(m*n)
     """
-    row = col = 0
-    going_across_cols = going_down = going_right = True
-    step = 0
-    flatten = []
-    left_border, right_border = 0, len(matrix[0])-1
-    lower_border, upper_border = len(matrix)-1, 0
-    while step < len(matrix[0])*len(matrix):  # iterate through all elements in matrix
-        flatten.append(matrix[row][col])
-        if going_across_cols:
+    step = row = col = 0
+    traverse_horizontally = going_down = going_right = True
+    left_border, right_border = 0, len(matrix[0]) - 1
+    lower_border, upper_border = len(matrix) - 1, 0
+    result = []
+    while step < len(matrix[0]) * len(matrix):
+        result.append(matrix[row][col])
+        if traverse_horizontally:
             if going_right:
-                if col + 1 > right_border:
+                if col + 1 > right_border:  # we reached right border, so switch direction to down-wise
                     row += 1
                     going_right = False
-                    going_across_cols = False
+                    traverse_horizontally = False
                     if step >= len(matrix[0]):
                         left_border += 1
-                else:
+                else:  # continue moving from left to right
                     col += 1
             else:
-                if col - 1 < left_border:
+                if col - 1 < left_border:  # we reached left border, so switch direction to up-wise
                     row -= 1
                     going_right = True
-                    going_across_cols = False
+                    traverse_horizontally = False
                     right_border -= 1
-                else:
+                else:  # continue moving from right to left
                     col -= 1
         else:
             if going_down:
-                if row + 1 > lower_border:
+                if row + 1 > lower_border:  # we reached lower border, so switch direction to left-wise
                     col -= 1
                     going_down = False
-                    going_across_cols = True
+                    traverse_horizontally = True
                     upper_border += 1
-                else:
+                else:  # continue moving down-wise
                     row += 1
             else:
-                if row - 1 < upper_border:
+                if row - 1 < upper_border:  # we reached upper border, so switch direction to right-wise
                     col += 1
                     going_down = True
-                    going_across_cols = True
+                    traverse_horizontally = True
                     lower_border -= 1
-                else:
+                else:  # continue moving up-wise
                     row -= 1
-        step += 1
+        step += 1  # update step variable
 
-    return flatten
+    return result
 
 
 if __name__ == '__main__':
@@ -66,4 +65,3 @@ if __name__ == '__main__':
     assert flatten_spirally(matrix=[[1], [2]]) == [1, 2]
     assert flatten_spirally(matrix=[[1, 2]]) == [1, 2]
     assert flatten_spirally(matrix=[[1, 2, 3], [6, 5, 4]]) == [1, 2, 3, 4, 5, 6]
-
