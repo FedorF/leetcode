ac4689aa1b337e4609e7e53cbdc50653d114c312
fed <fedor.fomichev@phystech.edu>
2023-11-25 23:00:34 -0800

    add top75 first problem solution

---
+++ LeetCode 75/Array and String/Easy/1768. Merge Strings Alternately/solution.py
@@ -0,0 +1,24 @@
+def merge_str(word1: str, word2: str) -> str:
+    last_ind = 0
+    result = ""
+    for a, b in zip(word1, word2):
+        result += a
+        result += b
+        last_ind += 1
+
+    if len(word1) > len(word2):
+        for i in range(last_ind, len(word1)):
+            result += word1[i]
+
+    if len(word2) > len(word1):
+        for i in range(last_ind, len(word2)):
+            result += word2[i]
+
+    return result
+
+
+if __name__ == '__main__':
+    assert merge_str(word1="abc", word2="pqr") == "apbqcr"
+    assert merge_str(word1="ab", word2="pqrs") == "apbqrs"
+    assert merge_str(word1="abcd", word2="pq") == "apbqcd"
+    assert merge_str("a", "b") == "ab"
